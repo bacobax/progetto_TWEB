2b11a73e375ccfe4e761482d21b125886ba9242a
bacobax <francesco.bassignana.info@gmail.com>
2023-11-18 13:42:32 +0100

    Animazioni + update navbar

---
+++ react-app/src/constants/navbarElements.tsx
@@ -0,0 +1,10 @@
+import {Elements} from "../components/navbar/types";
+import {FaHome} from "react-icons/fa";
+
+export const navbarElements: Elements = [
+    {
+       name: "Home",
+         path: "/",
+        icon: <FaHome />
+
+    }
