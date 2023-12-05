d5226dc87cd690515a17ad78643552ecfc751596
bacobax <francesco.bassignana.info@gmail.com>
2023-12-05 11:16:25 +0100

    creazione jpa server

---
+++ solution/serverSpring/springbootserver/src/test/java/com/progettotweb/springbootserver/SpringbootserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettotweb.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
